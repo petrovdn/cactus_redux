c3fefd2331b32fe6164aa7e5fe98fee7154b2a8c
Petrov Dmitry <petrov@lad24.ru>
2016-10-04 20:22:38 +0300

    0.0.3
    yes, redux is ready

--- src/cactus.js
+++ src/cactus.js
@@ -1,6 +1,5 @@
-import React from 'React'
+import React, {Component} from 'react'
 import {
-    AppRegistry,
     StyleSheet,
     View,
     Text } from 'react-native'
@@ -29,6 +28,16 @@ import {Provider} from 'react-redux'
  */
 import configureStore from './lib/configureStore'
 
+/**
+ * ### Translations
+ */
+var I18n = require('react-native-i18n')
+
+// Support fallbacks so en-US & en-BR both use en
+I18n.fallbacks = true
+
+import Translations from './lib/Translations'
+I18n.translations = Translations
     /**
      * ### containers
      *
@@ -123,85 +132,79 @@ class TabIcon extends React.Component {
      * will be used when doing hot loading
      */
 
-export default function native (platform) {
-  let cactus = React.createClass({
-    render () {
-      const store = configureStore(getInitialState())
+class cactus_redux extends Component {
+  render () {
+    const store = configureStore(getInitialState())
 
                 // configureStore will combine reducers from snowflake and main application
                 // it will then create the store based on aggregate state from all reducers
-          store.dispatch(setPlatform(platform))
-          store.dispatch(setVersion(VERSION))
-          store.dispatch(setStore(store))
+    // store.dispatch(setPlatform(platform))
+    store.dispatch(setVersion(VERSION))
+    store.dispatch(setStore(store))
 
                 // setup the router table with App selected as the initial component
                 // note: See https://github.com/aksonov/react-native-router-flux/issues/948
-          return (
+    return (
 
-            <Provider store={store}>
-              <Router sceneStyle={{ backgroundColor: 'white' }}>
-                <Scene key='root' hideNavBar>
-                  <Scene key='App'
-                    component={App}
-                    type='replace'
-                    initial />
+      <Provider store={store}>
+        <Router sceneStyle={{ backgroundColor: 'white' }}>
+          <Scene key='root' hideNavBar>
+            <Scene key='App'
+              component={App}
+              type='replace'
+              initial />
 
-                  <Scene key='InitialLoginForm'
-                    component={Register}
-                    type='replace' />
+            <Scene key='InitialLoginForm'
+              component={Register}
+              type='replace' />
 
-                  <Scene key='Login'
-                    component={Login}
-                    type='replace' />
+            <Scene key='Login'
+              component={Login}
+              type='replace' />
 
-                  <Scene key='Register'
-                    component={Register}
-                    type='replace' />
+            <Scene key='Register'
+              component={Register}
+              type='replace' />
 
-                  <Scene key='ForgotPassword'
-                    component={ForgotPassword}
-                    type='replace' />
+            <Scene key='ForgotPassword'
+              component={ForgotPassword}
+              type='replace' />
 
-                  <Scene key='Subview'
-                    component={Subview} />
+            <Scene key='Subview'
+              component={Subview} />
 
-                  <Scene key='Tabbar'
-                    tabs
-                    hideNavBar
-                    tabBarStyle={styles.tabBar}
-                    default='Main'>
+            <Scene key='Tabbar'
+              tabs
+              hideNavBar
+              tabBarStyle={styles.tabBar}
+              default='Main'>
 
-                    <Scene key='Logout'
-                      title={I18n.t('Snowflake.logout')}
-                      icon={TabIcon}
-                      iconName={"sign-out"}
-                      hideNavBar
-                      component={Logout} />
+              <Scene key='Logout'
+                title={I18n.t('Snowflake.logout')}
+                icon={TabIcon}
+                iconName={'sign-out'}
+                hideNavBar
+                component={Logout} />
 
-                    <Scene key='Main'
-                      title={I18n.t('Snowflake.main')}
-                      iconName={"home"}
-                      icon={TabIcon}
-                      hideNavBar
-                      component={Main}
-                      initial />
+              <Scene key='Main'
+                title={I18n.t('Snowflake.main')}
+                iconName={'home'}
+                icon={TabIcon}
+                hideNavBar
+                component={Main}
+                initial />
 
-                    <Scene key='Profile'
-                      title={I18n.t('Snowflake.profile')}
-                      icon={TabIcon}
-                      iconName={"gear"}
-                      hideNavBar
-                      component={Profile} />
-                  </Scene>
-                </Scene>
-              </Router>
-            </Provider>
+              <Scene key='Profile'
+                title={I18n.t('Snowflake.profile')}
+                icon={TabIcon}
+                iconName={'gear'}
+                hideNavBar
+                component={Profile} />
+            </Scene>
+          </Scene>
+        </Router>
+      </Provider>
           )
-        }
-      })
-        /**
-         * registerComponent to the AppRegistery and off we go....
-         */
-
-      AppRegistry.registerComponent('cactus', () => cactus)
-    }
+  }
+  }
+export default cactus_redux
